e8b0a16175dd1d73b0f3363d9170408a1c21fdc4
RedGrind <97067525+RedGrind@users.noreply.github.com>
2023-01-05 15:52:06 +0000

    Switched main.py to only have a topic based discussion, added more topics, and created sub-topics for every topic

--- main.py
+++ main.py
@@ -8,56 +8,46 @@ print()
 # First question
 # Asks user for a positive odd or even number, if not repeats
 def main_question(): 
-	starter_input = input("Enter an odd number for Q/A and an even number for a topic: ")
-	starter = int(starter_input)
-	print("---------------------------------------------------------------------")
-	print()
-	if (starter % 2 == 0):
-		topic_route()
-	elif (starter % 2 == 1): 
-		question_answer_route()
-	else:
-		main_question()
+    starter_input = input("Enter an even number for a topic: ")
+    starter = int(starter_input)
+    print("---------------------------------------------------------------------")
+    print()
+    if (starter % 2 == 0):
+        topic_route()
+    else:
+        main_question()
 
-def question_answer_route(): 
-	first_qa = input("What are your thoughts on the world cup")
-	print("That's great, I thought the world cup was amazing in my opinion")
+# Main topics and subtopics
+topics = {
+    "entertainment": ["soccer", "basketball", "football", "movies", "anime"],
+    "school": ["tuition", "grades", "studying", "stem", "humanities", "admission"],
+    "health": ["vaping", "smoking", "staying healthy", "habits"],
+    "technology": ["artificial intelligence", "machine learning", "chatgpt", "deep learning", "natural language processing"],
+    "business": ["nft", "affiliate marketing", "content creation", "tutoring", "online company"],
+    "food": ["asian", "italian", "diets", "mexican"],
+	"holiday": ["christmas", "mother's day", "thanksgiving", "memorial day", "new years"],
+	"games": ["roblox", "minecraft", "call of duty", "fortnite", "overwatch 2"]
+}
 
 def topic_route():
-	first_topic = input("What main topic do you want to talk about? [entertainment, school, heath, technology, business, & food]")
+    first_topic = input("What main topic do you want to talk about? " + str(list(topics.keys())))
+    if first_topic in topics:
+        sub_topic_route(first_topic)
+    else:
+        print("Sorry, I don't understand your input. Please choose a topic from the list.")
+        topic_route()
 
-main_topic = [
- "entertainment", "school", "health", "technology", "business", "food"
-]
-
-#Subtopics for each main topic
-entertainment_sub_topics = [
- "soccer", "basketball", "football", "movies", "anime"
-]
-school_sub_topics = [
- "tuition", "grades", "studying", "stem", "humanities", "admission"
-]
-health_sub_topics = ["vaping", "smoking", "staying healthy", "habits"]
-technology_sub_topics = [
- "artificial intelligence", "machine learning", "chatgpt", "deep learning",
- "natural language processing"
-]
-business_sub_topics = [
- "nft", "affiliate marketing", "content creation", "tutoring", "online company"
-]
-food_sub_topics = [
- "asian",
- "italian",
- "diets",
- "mexican",
-]
+def sub_topic_route(main_topic):
+    print("Here are the subtopics for " + main_topic + ": " + str(topics[main_topic]))
+    sub_topic = input("Which subtopic would you like to discuss? ")
+    if sub_topic in topics[main_topic]:
+        print("Let's talk about " + sub_topic + "!")
+    else:
+        print("Sorry, I don't understand your input. Please choose a subtopic from the list.")
+        sub_topic_route(main_topic)
 
 # Main loop that controls the chatbot
-want_to_exit = ''; 
+want_to_exit = ''
 while want_to_exit != "Y": 
-	main_question()
-	want_to_exit = input("Do you want to exit the chatbot? (Y/N)")		
-
-# Topic Based Route
-# Ask about a main topic
-
+    main_question()
+    want_to_exit = input("Do you want to exit the chatbot? (Y/N)")
